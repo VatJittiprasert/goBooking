946588d55effd971adc28f49dc08b2ae4cca849c
InkZ <Vatcharapong.j@hotmail.com>
2024-01-27 21:33:05 +0700

    converted to tmpl file

--- cmd/web/route.go
+++ cmd/web/route.go
@@ -27,6 +27,11 @@ func routes(app *config.AppConfig) http.Handler {
 	//todo routes
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/generals-quaters", handlers.Repo.Generals)
+	mux.Get("/majors-suite", handlers.Repo.Majors)
+	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Get("/contact", handlers.Repo.Contact)
+	mux.Get("/make-reservation", handlers.Repo.Reservation)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
